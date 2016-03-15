230bc947dd0ad150ad9e79e156fbdff4650f5055
Sunday Adefila <adefilaedward@gmail.com>
2016-03-15 11:50:41 +0100

    removed extra spaces

--- algorithms/linear_search.rb
+++ algorithms/linear_search.rb
@@ -1,18 +1,17 @@
 module Algorithms
   module Search
+    class Linear
 
-		class Linear
-
-			def search e, array
-				result = nil
-				array.each_with_index do |a, i|
-					if a == e
-						result = i
-					end
-				end
-				# array.index e #which algorithm is used for this?
-				result
-			end
-		end
-	end
+      def search e, array
+        result = nil
+        array.each_with_index do |a, i|
+          if a == e
+            result = i
+          end
+        end
+        # array.index e #which algorithm is used for this?
+        result
+      end
+    end
+  end
 end
