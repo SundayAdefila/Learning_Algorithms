1a10afdfb2ff4852ae518c809b7348256e5f7524
Sunday Adefila <adefilaedward@gmail.com>
2016-03-22 10:12:06 +0100

    knapsack problem algorithm completed

--- common_problems/knapsack.rb
+++ common_problems/knapsack.rb
@@ -18,7 +18,25 @@ module CommonProblems
         end
       end
 
-      cost_matrix[n-1][max_weight]
+      packed_items = get_items_name(object_array, cost_matrix)
+
+      {items: packed_items, value: cost_matrix[n-1][max_weight]}
+    end
+
+    def get_items_name(object_array, cost_matrix)
+      i = cost_matrix.size - 1
+      currentCost = cost_matrix[0].size - 1
+      items = Array.new
+
+      while(i >= 0 && currentCost >= 0)
+        if(i == 0 && cost_matrix[i][currentCost] > 0 ) || (cost_matrix[i][currentCost] != cost_matrix[i-1][currentCost])
+          items.push object_array[i]['name']
+          currentCost -= object_array[i]['weight']
+        end
+        i -= 1
+      end
+      items.join(', ')
+
     end
 
     # FORMULA V[i, w]
@@ -26,5 +44,7 @@ module CommonProblems
     # == 0 if i == 0 or w == 0
     # == V[i-1, w] if w[i] > W
     # == max(V[i-1, w], vi + V[i-1, w-w[i]])
+    #
+    # Inspiration: http://rosettacode.org/
   end
 end
--- unit_tests/common_problems.rb
+++ unit_tests/common_problems.rb
@@ -4,18 +4,18 @@ Dir['./common_problems/**/*.rb'].each { |f| require f }
 class AlgoSearchTest < MiniTest::Test
   def test_knapsack
     items = [
-      {'name'=>'', 'weight'=> 1, 'value'=> 1},
-      {'name'=>'', 'weight'=> 3, 'value'=> 4},
-      {'name'=>'', 'weight'=> 4, 'value'=> 5},
-      {'name'=>'', 'weight'=> 5, 'value'=> 7},
+      {'name'=>'shoe', 'weight'=> 1, 'value'=> 1},
+      {'name'=>'spray', 'weight'=> 3, 'value'=> 4},
+      {'name'=>'phone', 'weight'=> 4, 'value'=> 5},
+      {'name'=>'ball', 'weight'=> 5, 'value'=> 7},
     ]
-    assert_equal(9, CommonProblems::Knapsack.new.find(7, items) )
+    assert_equal({items: "phone, spray", value: 9}, CommonProblems::Knapsack.new.find(7, items) )
 
     items = [
-      {'name'=>'', 'weight'=> 3, 'value'=> 5},
-      {'name'=>'', 'weight'=> 2, 'value'=> 3},
-      {'name'=>'', 'weight'=> 1, 'value'=> 4},
+      {'name'=>'shirt', 'weight'=> 3, 'value'=> 5},
+      {'name'=>'brush', 'weight'=> 2, 'value'=> 3},
+      {'name'=>'fork', 'weight'=> 1, 'value'=> 4},
     ]
-    assert_equal(9, CommonProblems::Knapsack.new.find(5, items) )
+    assert_equal({items: "fork, shirt", value: 9}, CommonProblems::Knapsack.new.find(5, items) )
   end
 end
